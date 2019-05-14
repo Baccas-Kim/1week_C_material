2ffba17d8481b069d1a9619d4e59f51571245bec
DHKim <ultraraptor@naver.com>
2019-05-14 23:10:12 +0900

    inital commit!

---
+++ Day2_meterial/연산자_문제풀이/연산자_문제풀이/packing.c
@@ -0,0 +1,22 @@
+/*
+ * packing.c
+ */
+
+#include "wshop1.h"
+
+#ifdef PACKING
+
+int main(void)
+{
+	unsigned int packing = 0;
+	int id;
+	int type;
+	char gender;
+
+	printf("# 직원 정보 입력\n");
+	// TODO: 이곳에 코드를 작성하세요.
+
+	return 0;
+}
+
+#endif
